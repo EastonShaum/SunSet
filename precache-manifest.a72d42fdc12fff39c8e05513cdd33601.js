e2885cbeaff79fe12b835ca89c9f21450089ab62
Easton Shaum <eastonshaum@gmail.com>
2022-09-14 21:38:19 -0600

    Deploy React app to GitHub Pages

---
+++ precache-manifest.a72d42fdc12fff39c8e05513cdd33601.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4155d0f27bede7b3a8e",
+    "url": "/static/css/main.b4754733.chunk.css"
+  },
+  {
+    "revision": "a4155d0f27bede7b3a8e",
+    "url": "/static/js/main.a4155d0f.chunk.js"
+  },
+  {
+    "revision": "497dc6329578ae794410",
+    "url": "/static/js/1.497dc632.chunk.js"
+  },
+  {
+    "revision": "7e620dca084ff0947cc4",
+    "url": "/static/js/2.7e620dca.chunk.js"
+  },
+  {
+    "revision": "8f0e4a4ed6cd5b4c3692",
+    "url": "/static/js/runtime~main.8f0e4a4e.js"
+  },
+  {
+    "revision": "0e52a398a1a810b9302c110dae2e4d96",
+    "url": "/static/media/business-man-suit-png-1-1@2x.0e52a398.png"
+  },
+  {
+    "revision": "3d5b6f5fbf40d88b10a07924cd2f63dd",
+    "url": "/static/media/funnel-person-2-png-1-1@2x.3d5b6f5f.png"
+  },
+  {
+    "revision": "938dda76dc5995751a04432287796f0e",
+    "url": "/static/media/solar-panels-white-background-1-1@2x.938dda76.png"
+  },
+  {
+    "revision": "077a30edf2057fa53edb493b7f226c4b",
+    "url": "/static/media/marble-background-image-1-1@1x.077a30ed.png"
+  },
+  {
+    "revision": "73180f94433080f8e4823aa5bab9199b",
+    "url": "/index.html"
+  }
+];
